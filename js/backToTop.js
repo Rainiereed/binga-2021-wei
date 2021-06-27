5737bbba19fe2b3f7d580688bc3e2a92910d6dac
Wei Lu <wei.lu@hyperisland.se>
2021-06-27 17:16:34 +0200

    add back-to-top button and some modifications

---
+++ js/backToTop.js
@@ -0,0 +1,18 @@
+var mybutton = document.getElementById("back-to-top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 2100 || document.documentElement.scrollTop > 2100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 1340;
+  document.documentElement.scrollTop = 1340;
+}
